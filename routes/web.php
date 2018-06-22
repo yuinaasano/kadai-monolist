c03b0333fe3657cee9d815d5e10ca6ad3f813a03
yuina.asano <yuina.asano@rakuten.com>
2018-06-22 06:26:39 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/', 'WelcomeController@index');
+
+// User registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
