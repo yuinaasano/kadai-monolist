c7baeafe8b2b9e8d27d508337368b7fc72ea2734
yuina.asano <yuina.asano@rakuten.com>
2018-06-26 04:35:40 +0000

    have final

--- resources/views/items/have_button.blade.php
+++ resources/views/items/have_button.blade.php
@@ -1,5 +1,5 @@
 @if (Auth::user()->is_having($item->code))
-    {!! Form::open(['route' => 'item_user.dont_hav', 'method' => 'delete']) !!}
+    {!! Form::open(['route' => 'item_user.dont_have', 'method' => 'delete']) !!}
         {!! Form::hidden('itemCode', $item->code) !!}
         {!! Form::submit('Have', ['class' => 'btn btn-success']) !!}
     {!! Form::close() !!}
